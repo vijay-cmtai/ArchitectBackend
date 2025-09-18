18d479f90a6b444760378c1ee4ec21efc5ee70b0
vijay-cmtai <vijay.maurya@completrix.com>
2025-09-18 09:24:51 +0530

    Update cartModel.js

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,16 +1,25 @@
+// src/models/cartModel.js
+
 const mongoose = require("mongoose");
 
 const cartItemSchema = mongoose.Schema({
   productId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Product", 
+    ref: "Product",
     required: true,
   },
   name: { type: String, required: true },
   quantity: { type: Number, required: true, default: 1 },
+  // Original price of the product
   price: { type: Number, required: true },
+  // Sale price if the item is on sale
+  salePrice: { type: Number },
+  isSale: { type: Boolean, default: false },
+  // These fields will be copied from the Product model
+  taxRate: { type: Number, default: 0 },
+  discountPercentage: { type: Number, default: 0 },
   image: { type: String },
-  size: { type: String },
+  size: { type: String }, // Assuming size comes from the client
 });
 
 const cartSchema = mongoose.Schema(
@@ -19,7 +28,7 @@ const cartSchema = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
-      unique: true, 
+      unique: true,
     },
     items: [cartItemSchema],
   },
