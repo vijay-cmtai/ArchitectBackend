9e35c5a754625f8d260f3e658b7d8cb88f4e58cd
vijay-cmtai <vijay.maurya@completrix.com>
2025-11-07 09:20:43 +0530

    Add files via upload

---
+++ routes/notificationRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getUnreadCounts,
+  getNotifications,
+  markAsRead,
+} = require("../controllers/notificationController");
+
+const { protect, admin } = require("../middleware/authMiddleware");
+
+router.route("/counts").get(protect, admin, getUnreadCounts);
+router.route("/").get(protect, admin, getNotifications);
+router.route("/:id/read").put(protect, admin, markAsRead);
+
+module.exports = router;
