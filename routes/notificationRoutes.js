85cf5b1bcc500abb1be3d09127f4ed428a11cc95
vijay-cmtai <vijay.maurya@completrix.com>
2025-11-07 11:42:06 +0530

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -5,11 +5,10 @@ const {
   getNotifications,
   markAsRead,
 } = require("../controllers/notificationController");
-
 const { protect, admin } = require("../middleware/authMiddleware");
-
 router.route("/counts").get(protect, admin, getUnreadCounts);
 router.route("/").get(protect, admin, getNotifications);
 router.route("/:id/read").put(protect, admin, markAsRead);
 
 module.exports = router;
+
